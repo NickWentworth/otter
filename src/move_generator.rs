3c37fc167cc4dad716313a972d518c116c01bfda
Nick <nickwentworth123@gmail.com>
2023-05-27 17:54:52 -0400

    added queen move generation

--- src/move_generator.rs
+++ src/move_generator.rs
@@ -23,17 +23,30 @@ impl Direction {
 
     const DIAGONALS: [isize; 4] = [Self::NE, Self::NW, Self::SE, Self::SW];
     const STRAIGHTS: [isize; 4] = [Self::N, Self::E, Self::S, Self::W];
+    const ALL: [isize; 8] = [
+        Self::N,
+        Self::E,
+        Self::S,
+        Self::W,
+        Self::NE,
+        Self::NW,
+        Self::SE,
+        Self::SW,
+    ];
 }
 
+type DirectionAttackPair = (isize, [Bitboard; 64]);
+
 pub struct MoveGenerator {
-    diagonal_attacks: [[Bitboard; 64]; 4],
-    straight_attacks: [[Bitboard; 64]; 4],
+    diagonal_attacks: [DirectionAttackPair; 4],
+    straight_attacks: [DirectionAttackPair; 4],
+    all_attacks: [DirectionAttackPair; 8],
 }
 
 /// Right-shifts the bitboard by the specified `amount` (if positive)
 ///
 /// If `amount` is negative, a left-shift is applied with the same magnitude
-fn negative_shift_right(board: Bitboard, amount: isize) -> Bitboard {
+fn conditional_shift_right(board: Bitboard, amount: isize) -> Bitboard {
     if amount >= 0 {
         board >> amount
     } else {
@@ -43,8 +56,8 @@ fn negative_shift_right(board: Bitboard, amount: isize) -> Bitboard {
 
 impl MoveGenerator {
     pub fn new() -> MoveGenerator {
-        /// Only needed temporarily, generates the attack vector for all squares in a particular direction
-        fn calculate_attack_in_direction(direction_offset: isize) -> [Bitboard; 64] {
+        // Only needed temporarily, generates the attack ray for all squares in a particular direction
+        let calculate_attack_in_direction = |direction_offset: isize| -> DirectionAttackPair {
             let mut boards: [Bitboard; 64] = [0; 64];
 
             // go through each square in the board to fill it in
@@ -52,7 +65,7 @@ impl MoveGenerator {
                 // generate the initial square the piece is on
                 // and the square of the next attack, with the bitwise operation being handled for negative directions
                 let mut attack: Bitboard = MSB_BOARD >> square;
-                let mut next_attack: Bitboard = negative_shift_right(attack, direction_offset);
+                let mut next_attack: Bitboard = conditional_shift_right(attack, direction_offset);
 
                 // to tell if we are going off to the other side, the attack and next attack will be on the A and H file
                 while ((attack & FileBoundMask::A) | (next_attack & FileBoundMask::H) != 0)
@@ -60,20 +73,21 @@ impl MoveGenerator {
                 {
                     // if the next attack is valid (and not wrapping to the other side of the board), we can now advance to the next attack (for the while loop)
                     attack = next_attack;
-                    next_attack = negative_shift_right(attack, direction_offset);
+                    next_attack = conditional_shift_right(attack, direction_offset);
 
                     // add this attack to the boards at the current square
                     boards[square] |= attack;
                 }
             }
 
-            boards
-        }
+            (direction_offset, boards)
+        };
 
         MoveGenerator {
             // generate diagonal attack directions
             diagonal_attacks: Direction::DIAGONALS.map(calculate_attack_in_direction),
             straight_attacks: Direction::STRAIGHTS.map(calculate_attack_in_direction),
+            all_attacks: Direction::ALL.map(calculate_attack_in_direction),
         }
     }
 
@@ -85,6 +99,12 @@ impl MoveGenerator {
 
         let mut moves: Vec<Move> = Vec::new();
 
+        // fetch these once instead of generating for every piece
+        let active_color = board.active_color();
+        let same_color = board.active_color_board();
+        let oppositng_color = board.inactive_color_board();
+        let both_colors = same_color | oppositng_color;
+
         // iterate through each type of piece
         for piece in [Pawn, Knight, Bishop, Rook, Queen, King] {
             // get the bitboard representing the pieces that can move of this type
@@ -93,35 +113,42 @@ impl MoveGenerator {
             // go through each position that this piece occurs in and pop it from the pieces bitboard
             while pieces_board != 0 {
                 let from = pop_msb_1(&mut pieces_board);
-                let position = MSB_BOARD >> from;
+                let piece_position = MSB_BOARD >> from;
 
                 // generate the correct move bitboard
                 let mut moves_board = match piece {
-                    Pawn => match board.active_color() {
+                    Pawn => match active_color {
                         Color::White => Self::generate_white_pawn_moves(
-                            position,
-                            board.active_color_board(),
-                            board.inactive_color_board(),
+                            piece_position,
+                            same_color,
+                            oppositng_color,
                         ),
                         Color::Black => Self::generate_black_pawn_moves(
-                            position,
-                            board.active_color_board(),
-                            board.inactive_color_board(),
+                            piece_position,
+                            same_color,
+                            oppositng_color,
                         ),
                     },
-                    Knight => Self::generate_knight_moves(position, board.active_color_board()),
-                    Bishop => self.generate_bishop_moves(
-                        position,
-                        board.active_color_board() | board.inactive_color_board(),
-                        board.active_color_board(),
-                    ), // TODO - generate sliding piece moves
-                    Rook => self.generate_rook_moves(
-                        position,
-                        board.active_color_board() | board.inactive_color_board(),
-                        board.active_color_board(),
-                    ), // TODO - generate sliding piece moves
-                    Queen => 0, // TODO - generate sliding piece moves
-                    King => Self::generate_king_moves(position, board.active_color_board()),
+                    Knight => Self::generate_knight_moves(piece_position, same_color),
+                    Bishop => Self::generate_sliding_moves(
+                        piece_position,
+                        both_colors,
+                        same_color,
+                        &self.diagonal_attacks,
+                    ),
+                    Rook => Self::generate_sliding_moves(
+                        piece_position,
+                        both_colors,
+                        same_color,
+                        &self.straight_attacks,
+                    ),
+                    Queen => Self::generate_sliding_moves(
+                        piece_position,
+                        both_colors,
+                        same_color,
+                        &self.all_attacks,
+                    ),
+                    King => Self::generate_king_moves(piece_position, same_color),
                 };
 
                 // and similarly pop each bit from the bitboard, pushing a move to the list as we go
@@ -269,34 +296,34 @@ impl MoveGenerator {
         forward_move | double_move | valid_attacks
     }
 
-    /// Bishop move generation generally works as follows:
+    /// Sliding piece move generation generally works as follows:
     ///
-    /// 1. Index the pre-generated 2D array by the direction of attack and the square the bishop is on
-    /// 2. Bitwise AND the attack ray and all pieces to find all pieces blocking this bishop
-    /// 3. Find the index of the nearest blocker to the bishop and clip the attack off at that piece
+    /// 1. Index the pre-generated 2D array by the direction of attack and the square the attacking piece is on
+    /// 2. Bitwise AND the attack ray and all pieces to find the pieces blocking the attacking piece
+    /// 3. Find the index of the nearest blocker to the attacking piece and clip the attack off at that piece
     /// 4. Make sure the first blocker is not a piece of the same color, if it is remove that square
-    fn generate_bishop_moves(
-        &self,
-        bishop_position: Bitboard,
+    fn generate_sliding_moves(
+        piece_position: Bitboard,
         all_pieces: Bitboard,
         same_color_pieces: Bitboard,
+        attacks: &[DirectionAttackPair],
     ) -> Bitboard {
         // get the square this bishop is on to index attack direction arrays
-        let bishop_square = bishop_position.leading_zeros() as usize;
+        let piece_square = piece_position.leading_zeros() as usize;
 
         let mut moves: Bitboard = 0;
 
         // go through the directions and attacks associated with each direction
-        for (direction, attacks) in Direction::DIAGONALS.into_iter().zip(self.diagonal_attacks) {
-            // by AND-ing the bishop's attack with all pieces, we get the pieces that block this attack
-            let blocker_board = attacks[bishop_square] & all_pieces;
+        for (direction, attacks) in attacks {
+            // by AND-ing the piece's attack with all pieces, we get the pieces that block this attack
+            let blocker_board = attacks[piece_square] & all_pieces;
 
             let clipped_attack = if blocker_board == 0 {
                 // if there are no pieces blocking, then the entire attack direction is kept
-                attacks[bishop_square]
+                attacks[piece_square]
             } else {
                 // else, find the first piece in the blocking direction
-                let first_blocker = if direction > 0 {
+                let first_blocker = if *direction > 0 {
                     // if the direction is southward, the first piece will be closest to the MSB
                     blocker_board.leading_zeros() as usize
                 } else {
@@ -305,7 +332,7 @@ impl MoveGenerator {
                 };
 
                 // finally, XOR the attack with the same direction attack from this first blocker to clip it off after the blocker
-                attacks[bishop_square] ^ attacks[first_blocker]
+                attacks[piece_square] ^ attacks[first_blocker]
             };
 
             // add this attack direction to the moves bitboard
@@ -313,49 +340,6 @@ impl MoveGenerator {
         }
 
         // since all pieces are used to find blockers, this bishop may be attacking a same-color piece
-        // this AND will take the possibly invalid final move in the slide and see if it shares a space with a piece of the same color
-        moves & !same_color_pieces
-    }
-
-    /// Nearly identical to bishop move generation, only change is that straight attacks are used instead of diagonals
-    fn generate_rook_moves(
-        &self,
-        rook_position: Bitboard,
-        all_pieces: Bitboard,
-        same_color_pieces: Bitboard,
-    ) -> Bitboard {
-        // get the square this rook is on to index attack direction arrays
-        let rook_square = rook_position.leading_zeros() as usize;
-
-        let mut moves: Bitboard = 0;
-
-        // go through the directions and attacks associated with each direction
-        for (direction, attacks) in Direction::STRAIGHTS.into_iter().zip(self.straight_attacks) {
-            // by AND-ing the rook's attack with all pieces, we get the pieces that block this attack
-            let blocker_board = attacks[rook_square] & all_pieces;
-
-            let clipped_attack = if blocker_board == 0 {
-                // if there are no pieces blocking, then the entire attack direction is kept
-                attacks[rook_square]
-            } else {
-                // else, find the first piece in the blocking direction
-                let first_blocker = if direction > 0 {
-                    // if the direction is southward, the first piece will be closest to the MSB
-                    blocker_board.leading_zeros() as usize
-                } else {
-                    // else the first piece will be closest to the LSB (and subtract 63 because we need it in terms of MSB, not LSB)
-                    63 - blocker_board.trailing_zeros() as usize
-                };
-
-                // finally, XOR the attack with the same direction attack from this first blocker to clip it off after the blocker
-                attacks[rook_square] ^ attacks[first_blocker]
-            };
-
-            // add this attack direction to the moves bitboard
-            moves |= clipped_attack;
-        }
-
-        // since all pieces are used to find blockers, this rook may be attacking a same-color piece
         // this AND will take the possibly invalid final move in the slide and see if it shares a space with a piece of the same color
         moves & !same_color_pieces
     }
